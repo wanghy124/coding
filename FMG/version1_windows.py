5b1be6a651e2d224711fe61d4c33e068c90acec3
wanghy124 <wanghy124@163.com>
2020-03-08 20:57:32 +0800

    ver 1.1

--- FMG/version1_windows.py
+++ FMG/version1_windows.py
@@ -21,12 +21,16 @@ response_get_fgt = session_fgt.get(url_fgt_vpn)
 
 device_info_fgt = response_get_fgt.text.split('proxyid')[1:]
 device_pool_fgt = {}
+nat_ip_pool_fgt = []
 
 for j in device_info_fgt:
     nat_ip_fgt = re.findall('.*"subnet":"(10\.162\.\d{1,3}\.\d{1,3})-.*', j)
     tun_ip_fgt = re.findall('.*"subnet":"100\.(10[1-2])\.\d{1,3}\.\d{1,3}-.*', j)
     if tun_ip_fgt:
         device_pool_fgt[nat_ip_fgt[0]] = tun_ip_fgt[0]
+    if nat_ip_fgt:
+        nat_ip_pool_fgt.append(nat_ip_fgt[0])
+
 
 # --------------------登录FMG--------------------
 
@@ -52,12 +56,20 @@ response_get_fmg = requests.request("POST", url_fmg, headers=headers, data=paylo
 
 device_info_fmg = response_get_fmg.text.split('adm_pass')
 
+only_4g_fgt = ["FGTCE5001", "FGTEA5256", "FGTEA5235", "FGTCE5141", "FGTWE5267", "FGTCE5151", "FGTNE5036",
+          "FGTEA5424L", "FGTCE5251", "FGTNE5034", "FGTGB5220", "FGTGB5042", "FGTNE5149", "FGTWE5153", "FGTGB5357L",
+          "FGTCE5583L", "FGTGB5194", "FGTGB5087", "FGTEA5032", "FGTGB5567L", "FGTEA5708L", "FGTCE5357L", "FGTSO5186",
+          "FGTSO5071", "FGTEA5182", "FGTGB5210", "FGTEA5300", "FGTCE5100", "FGTCE5395L", "FGTGB5034", "FGTWE5314L",
+          "FGTEA5291", "FGTEA5370L", "FGTCE5518L", "FGTGB5323", "FGTGB51019L", "FGTGB5425L", "FGTCE5027", "FGTEA5138",
+          "FGTGB5024", "FGTGB5258", "FGTGB5020", "FGTEA5106", "FGTGB5428L", "FGTGB5366L", "FGTGB5298", "FGTWE5450L",
+          "FGTWE5060", "FGTGB5337"]
+
 now = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime(time.time()))
 fname = now + r"_fmg_report.csv"
 
 with open(fname, "w", newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Name", "Status"])
+    writer.writerow(["Name", "Status", "Reamrk"])
     for i in device_info_fmg:
         hostname = re.findall('.*"hostname":\s+"(FGT\w+)",\s+.*', i)
         conn_status = re.findall('.*"conn_status":\s+(\d),\s+.*', i)
@@ -68,11 +80,20 @@ with open(fname, "w", newline='') as csvfile:
             if conn_status[0] == '1' and value_tun_ip == '101':
                 writer.writerow([hostname[0], 'Good'])
             elif conn_status[0] == '1' and value_tun_ip == '102':
-                writer.writerow([hostname[0], '4G'])
+                if hostname[0] in only_4g_fgt:
+                    writer.writerow([hostname[0], '4G', 'Only 4G'])
+                else:
+                    writer.writerow([hostname[0], '4G'])
             elif conn_status[0] == '1' and value_tun_ip == 65535:
-                writer.writerow([hostname[0], 'Add tunnel IP!!!'])
+                if nat_ip_fmg[0] in nat_ip_pool_fgt:
+                    writer.writerow([hostname[0], 'Add tunnel IP!!!'])
+                else:
+                    writer.writerow([hostname[0], 'VPN Error!!!'])
             elif conn_status[0] == '2':
-                writer.writerow([hostname[0], 'Offline!'])
+                if hostname[0] in only_4g_fgt:
+                    writer.writerow([hostname[0], 'Offline!', 'Only 4G'])
+                else:
+                    writer.writerow([hostname[0], 'Offline!'])
             else:
                 writer.writerow([hostname[0], 'Null'])
 
