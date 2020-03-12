94dc543c4882edd862ae56f7cd9a7dd09fc8b821
wanghy124 <wanghy124@163.com>
2020-03-12 23:42:02 +0800

    c11

--- chapter3/c11_2.py
+++ chapter3/c11_2.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
 import logging
 logging.getLogger('kamene.runtime').setLevel(logging.ERROR)
 from kamene.all import *
--- chapter3/c11_3.py
+++ chapter3/c11_3.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
 from c11_2.py import *
 
 a = pings('192.168.27.2')
