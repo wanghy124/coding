f162410a0c23ee8458dbb37f09fd1b154f7f49b0
wanghy124 <wanghy124@163.com>
2020-03-14 19:40:26 +0800

    c13

--- chapter3/c13_1.py
+++ chapter3/c13_1.py
@@ -12,10 +12,20 @@ class ping:
     def size(self, length):
         self.length = length
     def ping1(self):
-        ping_pkt = IP(dst=self.dstip, src=self.srcip)/ICMP()/(self.length)
+        ping_pkt = IP(dst=self.dstip, src=self.srcip)/ICMP()/('a'*self.length)
         ping_result = sr1(ping_pkt, timeout=2, verbose=False)
         if ping_result:
             print(self.dstip, 'Yes')
         else:
             print(self.dstip, 'No')
+    def ping5(self):
+        ping_pkt = IP(dst=self.dstip, src=self.srcip)/ICMP()/('a'*self.length)
+        for i in range(5):
+            ping_result = sr1(ping_pkt, timeout=2, verbose=False)
+            if ping_result:
+                print('!', end='', flush=True)
+            else:
+                print('.', end='', flush=True)
+
+
 
--- chapter3/c13_2.py
+++
@@ -1,5 +0,0 @@
-from  c13_1 import *
-
-pingtest = ping('192.168.27.2')
-pingtest.ping1()
-
