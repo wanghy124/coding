5a9801b2724eb259dce21261e968f2e66d1d04d5
wanghy124 <wanghy124@163.com>
2021-04-14 00:08:25 +0800

    ssh tool

--- i_hate_work/rconfig_tool.py
+++ i_hate_work/rconfig_tool.py
@@ -1,37 +1,27 @@
-import paramiko
-import time
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
 import os
 import re
 
-def ssh_tool(ip, username, password, verbose=True):
-    ssh = paramiko.SSHClient()  # 创建SSH Client
-    ssh.load_system_host_keys()  # 加载系统SSH密钥
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 添加新的SSH密钥
-    ssh.connect(ip, port=22, username=username, password=password, timeout=10, compress=True, allow_agent=False,
-                look_for_keys=False)  # SSH连接
+cfg_name_list = ['"showstartup-config*.txt"', '"show-*.txt"', '"showconfiguration-*.txt"']
+cfg_path_list_new = []
 
-    chan = ssh.invoke_shell()  # 激活交互式shell
-    time.sleep(1)
+for k in cfg_name_list:
+    cfg_path = os.popen('find /home/rconfig/data -name ' + k + ' | sort').read()
+    cfg_path_list = cfg_path.split('\n')
 
-    ap_list = []
+    for i in range(len(cfg_path_list)):
+        if i<len(cfg_path_list)-1:
+            device_name_1 = re.findall('/home/rconfig/data/[-\w\s]+/([-\w\s]+)/.*',cfg_path_list[i])
+            device_name_2 = re.findall('/home/rconfig/data/[-\w\s]+/([-\w\s]+)/.*',cfg_path_list[i+1])
+            if device_name_1 == device_name_2:
+                continue
+            else:
+                cfg_path_list_new.append(cfg_path_list[i])
+        else:
+            cfg_path_list_new.append(cfg_path_list[i])
 
-    chan.send(username)
-    chan.send(b'\n')
-    chan.send(password)
-    chan.send(b'\n')
-    time.sleep(3)
-    chan.send(b'sudo -i')
-    chan.send(b'\n')
-    time.sleep(1)
-    chan.send(b'find /home/rconfig/data -name "showstartup-config*.txt"')
-    chan.send(b'\n')
-    time.sleep(3)
-    output = chan.recv(200000).decode()  # 读取回显，有些回想可能过长，请把接收缓存调大
-    if verbose:
-        print(output)  # 打印回显
-
-    file_list = str(output).split('\n')
-    for i in file_list:
-        hostname = re.match('(CN[-A-Za-z0-9]*)\s+', i)
-        if hostname:
-            ap_list.append(ap[0])
+for j in cfg_path_list_new:
+    if j:
+        os.popen("cat " + j + " | sed -i ':label;N;s/\\n/ /;b label' " + j)
